15ee33ce31b8a84f746170d1fc0118cda5ec4da0
ileanaspl <ileana.saintpaul@gmail.com>
2024-06-12 11:40:20 +0200

    feat: add a map

---
+++ client/src/components/Map/Map.jsx
@@ -0,0 +1,18 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+function Map() {
+  return (
+    <div>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        style={{ height: "400px" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </MapContainer>
+    </div>
+  );
+}
+
+export default Map;
