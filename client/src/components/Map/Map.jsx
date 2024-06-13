e5cd363d07e3dcd088ae0ceace40f719a7fef1f4
ileanaspl <ileana.saintpaul@gmail.com>
2024-06-13 16:55:05 +0200

    feat: add marker to the map

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,16 +1,21 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import "../../services/tools/markerCoordinates.json";
+import markers from "../../services/tools/markerCoordinates.json";
 
 function Map() {
   return (
     <div>
       <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        style={{ height: "400px" }}
+        center={[43.605856, 1.443761]}
+        zoom={14}
+        style={{ height: "100dvh" }}
       >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png" />
+        {markers.map((marker) => (
+          <Marker key={marker.id} position={marker.geocode}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
