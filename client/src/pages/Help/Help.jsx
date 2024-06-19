144f015be570e320accec015509e7bc3f4cf2b07
RomainRG31 <romain32.roger@gmail.com>
2024-06-19 11:17:20 +0200

    feat: merge

--- client/src/pages/Help/Help.jsx
+++ client/src/pages/Help/Help.jsx
@@ -1,9 +1,13 @@
+import Footer from "../../components/Footer/Footer";
+import NavBar from "../../components/NavBar/NavBar";
 import "./Help.scss";
 
 function Help() {
   return (
     <div>
+      <NavBar />
       <p>Help</p>
+      <Footer />
     </div>
   );
 }
