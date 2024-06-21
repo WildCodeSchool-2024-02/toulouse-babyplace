fbcd5330c1874d0056fdad3fecb72fd301d118b2
ileanaspl <ileana.saintpaul@gmail.com>
2024-06-21 12:14:44 +0200

    fix: correction style

--- client/src/pages/SignUpPro/SignUpProLocalisation/SignUpProLocalisation.jsx
+++ client/src/pages/SignUpPro/SignUpProLocalisation/SignUpProLocalisation.jsx
@@ -4,21 +4,23 @@ import "./SignUpProLocalisation.scss";
 
 function SignUpProLocalisation() {
   return (
-    <form>
-      <progress max="100" value="10" />
-      <Input
-        type="text"
-        title="Adresse postale"
-        placeholder="N° et nom de rue"
-      />
-      <Input type="text" title="Code postal" placeholder="Code Postal" />
-      <Input type="text" title="Ville" placeholder="Ville" />
-      <Button
-        label="Suivant"
-        nextPath="/sign-up-pro/images"
-        previousPath="/sign-up-pro/structure"
-      />
-    </form>
+    <div>
+      <progress max="100" value="20" />
+      <form>
+        <Input
+          type="text"
+          title="Adresse postale"
+          placeholder="N° et nom de rue"
+        />
+        <Input type="text" title="Code postal" placeholder="Code Postal" />
+        <Input type="text" title="Ville" placeholder="Ville" />
+        <Button
+          label="Suivant"
+          nextPath="/sign-up-pro/images"
+          previousPath="/sign-up-pro/structure"
+        />
+      </form>
+    </div>
   );
 }
 
