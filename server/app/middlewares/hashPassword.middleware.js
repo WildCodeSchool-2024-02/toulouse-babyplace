aaabab584138841ec48d531d40aabda23522d6dc
ileanaspl <ileana.saintpaul@gmail.com>
2024-06-26 16:53:11 +0200

    feat: add hasing options

--- server/app/middlewares/hashPassword.middleware.js
+++ server/app/middlewares/hashPassword.middleware.js
@@ -4,8 +4,15 @@ const hashPassword = async (req, res, next) => {
   const user = req.body;
   const { password } = user;
 
+  const hashingOptions = {
+    type: argon2.argon2id,
+    memoryCost: 19 * 2 ** 10,
+    timeCost: 2,
+    parallelism: 1,
+  };
+
   try {
-    const hashedPassword = await argon2.hash(password);
+    const hashedPassword = await argon2.hash(password, hashingOptions);
     user.password = hashedPassword;
   } catch (error) {
     next(error);
