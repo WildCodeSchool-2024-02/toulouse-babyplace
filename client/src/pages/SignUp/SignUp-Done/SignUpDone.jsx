176832c37dab2b637f5ed89307ae121a4465d6cd
PierreVir <pierre.virama@gmail.com>
2024-06-26 16:56:34 +0200

    feat: auth on going

---
+++ client/src/pages/SignUp/SignUp-Done/SignUpDone.jsx
@@ -0,0 +1,13 @@
+
+function SignUpDone() {
+  return (
+    <div>
+      <p className="done">
+        Votre inscription est validé ✅, vous allez etre notifier de la suite
+        des étapes !{" "}
+      </p>
+    </div>
+  );
+}
+
+export default SignUpDone;
