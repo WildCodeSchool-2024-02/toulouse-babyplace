44dbf070e0913822f38711e3a4d269e54f4b140f
PierreVir <pierre.virama@gmail.com>
2024-06-27 09:24:33 +0200

    feat: authentification on going in signup page

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,40 +1,94 @@
 import "./SignUp.scss";
 
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function SignUp() {
+  const emailRef = useRef();
+
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Les mots de passe ne correspondent pas !");
+      return;
+    }
+
+    if (password.length < 8) {
+      alert("Le mot de passe doit comporter au moins 8 caractères !");
+      return;
+    }
+
+    if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])/.test(password)) {
+      alert(
+        "Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial !"
+      );
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.APP_SECRET}/api/customers`,
+        {
+          method: "post",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify({
+            email: emailRef.current.value,
+            password,
+          }), // action POST sur register => envoyer le body register (req.body.register?)
+        }
+      );
+
+      if (response.status === 201) {
+        navigate("/sign-up");
+      } else {
+        console.info(response);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="inscriptions">
       <form
         className="formulaire"
-        action="fichierDeLiaisonVersLaBDD"
         method="post"
+        onSubmit={handleSubmit}
+        action="/sign-up-done"
       >
         <h2>Création de Compte</h2>
-        <div className="genre">
-          <legend>Genre</legend>
-          <input type="radio" id="femme" name="sexe" value="femme" />
-          <label htmlFor="femme">Femme</label>
-          <input type="radio" id="homme" name="sexe" value="homme" />
-          <label htmlFor="homme">Homme</label>
-          <br />
-        </div>
-
         <label htmlFor="nom">Nom</label>
-        <input className="champ" type="text" placeholder="Nom" id="nom" />
+        <input className="champ" type="text" placeholder="Nom" id="name" />
         <label htmlFor="prenom">Prénom</label>
         <input
           className="champ"
           type="text"
           placeholder="Prénom"
           name="prenom"
-          id="prenom"
+          id="firstname"
         />
         <label htmlFor="email">Email</label>
         <input
           className="champ"
           type="email"
           placeholder="Email"
+          ref={emailRef}
           name="email"
-          id="email"
+          id="mail"
         />
         <label htmlFor="phone-number">Numero de Telephone</label>
         <input
@@ -42,21 +96,23 @@ function SignUp() {
           type="tel"
           placeholder="Numero de Telephone"
           name="phone-number"
-          id="phone-number"
+          id="phone"
         />
 
         <label htmlFor="password">Mot de Passe</label>
         <input
-          className="champ"
           type="password"
-          placeholder="Mot de Passe"
           id="password"
+          value={password}
+          onChange={handlePasswordChange}
         />
         <label htmlFor="confirm-password">Confirmer votre mot de Passe</label>
         <input
           className="champ"
           type="password"
           placeholder="Confirmer votre mot de Passe"
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
           id="confirm-password"
         />
         <label htmlFor="dateofbirth">Date de naissance</label>
