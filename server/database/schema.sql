f478871ceaa87161f47ed8a76fed30aed5542820
RomainRG31 <romain32.roger@gmail.com>
2024-07-15 10:57:13 +0200

    feat: restructuration of tables

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -2,60 +2,52 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Table `lumen`.`childcare_center`
--- -----------------------------------------------------
 CREATE TABLE childcare_center (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `description` VARCHAR(200) NOT NULL,
-  `phone` VARCHAR(100),
-  `street_address` VARCHAR(100) NOT NULL,
-  `zip_code` VARCHAR(20) NOT NULL,
-  `city` VARCHAR(100) NOT NULL,
-  `mail` VARCHAR(100) NOT NULL,
-  `password` TEXT NOT NULL,
-  `url` TEXT NOT NULL,
-  `opening` TIME NOT NULL,
-  `closing` TIME NOT NULL,
-  `capacity` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `idprofessional_UNIQUE` (`id` ASC) VISIBLE)
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  description VARCHAR(200) NOT NULL,
+  phone VARCHAR(100),
+  street_address VARCHAR(100) NOT NULL,
+  zip_code VARCHAR(20) NOT NULL,
+  city VARCHAR(100) NOT NULL,
+  mail VARCHAR(100) NOT NULL,
+  password TEXT NOT NULL,
+  url TEXT NOT NULL,
+  opening TIME NOT NULL,
+  closing TIME NOT NULL,
+  capacity INT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX idprofessional_UNIQUE (id ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
--- -----------------------------------------------------
--- Table `lumen`.`customer`
--- -----------------------------------------------------
 CREATE TABLE customer (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `hashed_password` VARCHAR(255) NOT NULL,
-  `name` VARCHAR(100) NOT NULL,
-  `firstname` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `address` VARCHAR(150) NOT NULL,
-  `phone` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `idcustomer_UNIQUE` (`id` ASC) VISIBLE)
+  id INT NOT NULL AUTO_INCREMENT,
+  hashed_password VARCHAR(255) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  firstname VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  address VARCHAR(150) NOT NULL,
+  phone VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX idcustomer_UNIQUE (id ASC) VISIBLE)
 ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Table `lumen`.`childcare_center_customer`
--- -----------------------------------------------------
+
 CREATE TABLE childcare_center_customer (
-  `childcare_center_id` INT NOT NULL,
-  `customer_id` INT NOT NULL,
-  PRIMARY KEY (`childcare_center_id`, `customer_id`), 
-  INDEX `fk_childcare_center_idx` (`childcare_center_id` ASC) VISIBLE, 
-  INDEX `fk_customer_idx` (`customer_id` ASC) VISIBLE,
-  CONSTRAINT `fk_childcare_center` 
-    FOREIGN KEY (`childcare_center_id`)
-    REFERENCES `lumen`.`childcare_center` (`id`)
+  childcare_center_id INT NOT NULL,
+  customer_id INT NOT NULL,
+  PRIMARY KEY (childcare_center_id, customer_id), 
+  INDEX fk_childcare_center_idx (childcare_center_id ASC) VISIBLE, 
+  INDEX fk_customer_idx (customer_id ASC) VISIBLE,
+  CONSTRAINT fk_childcare_center 
+    FOREIGN KEY (childcare_center_id)
+    REFERENCES lumen.childcare_center (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_customer` 
-    FOREIGN KEY (`customer_id`)
-    REFERENCES `lumen`.`customer` (`id`)
+  CONSTRAINT fk_customer 
+    FOREIGN KEY (customer_id)
+    REFERENCES lumen.customer (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
