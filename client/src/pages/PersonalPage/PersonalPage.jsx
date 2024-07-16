0e5627fbf840b20ce588d574f96c89bd21d98c7a
PierreVir <pierre.virama@gmail.com>
2024-07-16 11:34:40 +0200

    feat: commit for pull

--- client/src/pages/PersonalPage/PersonalPage.jsx
+++ client/src/pages/PersonalPage/PersonalPage.jsx
@@ -1,9 +1,17 @@
+import Calendar from "../../components/Calendar/Calendar";
 import TableComponent from "../../components/WaitingList/TableComponent";
 
 function PersonalPage() {
   return (
     <div>
       <TableComponent />
+      <div className="profile">
+        <p>
+          Samantha Doe
+          <Calendar />
+        </p>
+
+      </div>
     </div>
   );
 }
