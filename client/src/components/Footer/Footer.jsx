a4f9626c70f32658a99473a129e87a333b2141fa
RomainRG31 <romain32.roger@gmail.com>
2024-07-22 15:50:58 +0200

    feat: add some modifs on footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,20 +1,12 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "./Footer.scss";
 
 function Footer() {
+  const location = useLocation();
+  const isSearchPage = location.pathname.toLowerCase() === "/search";
+
   return (
     <div id="footer">
-      <div id="footer-top">
-        <h2>Essayez c’est gratuit !</h2>
-        <div>
-          <button id="btn-more" type="button">
-            En savoir plus
-          </button>
-          <button id="btn-demo" type="button">
-            Demander une démo <span>&#10140;</span>
-          </button>
-        </div>
-      </div>
       <div>
         <p>Lumen</p>
         <ul>
@@ -27,9 +19,11 @@ function Footer() {
           <NavLink to="/sign-in-pro">
             <li>Lumen Pro</li>
           </NavLink>
-          <NavLink to="/sign-in">
-            <li>Se connecter</li>
-          </NavLink>
+          {!isSearchPage && (
+            <NavLink to="/sign-in">
+              <li>Se connecter</li>
+            </NavLink>
+          )}
         </ul>
       </div>
       <p id="p">© 2024 | Tous droits réservés</p>
