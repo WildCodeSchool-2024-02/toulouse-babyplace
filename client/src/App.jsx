c5396fab1a937a30b81b0dd8973ed5d85b59a7a0
ileanaspl <ileana.saintpaul@gmail.com>
2024-07-23 09:22:23 +0200

    fix: change all nourices into assistantes maternelles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,20 +11,21 @@ function App() {
             <div id="lumen">
               <h1>Lumen</h1>
               <p>
-                Découvrez la solution idéale pour trouver la nourrice parfaite
-                pour vos enfants.
+                Découvrez la solution idéale pour trouver l'assistante
+                maternelle parfaite pour vos enfants.
               </p>
               <p>
-                Créez un profil sur Lumen et accédez à une communauté de
-                nourrices disponibles près de chez vous.
+                Créez un profil sur Lumen et accédez à une communauté
+                d'assistantes maternelles disponibles près de chez vous.
               </p>
               <ul>
                 <li>
-                  Créez un compte gratuitement pour vous ou votre nourrice.
+                  Créez un compte gratuitement pour vous ou votre assistante
+                  maternelle.
                 </li>
                 <li>
-                  Service facile à utiliser pour simplifier la recherche de
-                  nourrices selon vos horaires.
+                  Service facile à utiliser pour simplifier la recherche
+                  d'assistantes maternelles selon vos horaires.
                 </li>
                 <li>
                   Profils détaillés pour une meilleure visibilité et confiance.
@@ -40,13 +41,13 @@ function App() {
           <img src={easyLife} alt="easy-life images" />
           <div id="reservation">
             <h3>
-              Facilitez vos recherches de nourrices grâce à une plateforme
-              intuitive.
+              Facilitez vos recherches d'assistantes maternelles grâce à une
+              plateforme intuitive.
             </h3>
             <p>
-              Consultez les profils des nourrices disponibles en fonction de vos
-              besoins et horaires. Trouvez rapidement et facilement une nourrice
-              de confiance.
+              Consultez les profils des assistantes maternelles disponibles en
+              fonction de vos besoins et horaires. Trouvez rapidement et
+              facilement une assistante maternelle de confiance.
             </p>
           </div>
         </div>
@@ -55,8 +56,8 @@ function App() {
       <div id="managment">
         <div id="logiciel">
           <h3>
-            Profitez du meilleur service de mise en relation avec des nourrices
-            qualifiées
+            Profitez du meilleur service de mise en relation avec des
+            assistantes maternelles qualifiées
           </h3>
         </div>
         <div id="case-middle">
@@ -66,8 +67,8 @@ function App() {
                 <h4>Créez votre profil</h4>
                 <p>
                   Inscrivez-vous et créez un profil détaillé pour augmenter
-                  votre visibilité auprès des parents à la recherche de
-                  nourrices.
+                  votre visibilité auprès des parents à la recherche
+                  d'assistantes maternelles.
                 </p>
               </div>
             </div>
@@ -75,8 +76,9 @@ function App() {
               <div>
                 <h4>Recherchez facilement</h4>
                 <p>
-                  Utilisez nos filtres de recherche pour trouver des nourrices
-                  disponibles selon vos horaires et besoins spécifiques.
+                  Utilisez nos filtres de recherche pour trouver des assistantes
+                  maternelles disponibles selon vos horaires et besoins
+                  spécifiques.
                 </p>
               </div>
             </div>
@@ -94,8 +96,8 @@ function App() {
                 <h4>Informations claires</h4>
                 <p>
                   Les parents peuvent consulter des profils détaillés pour faire
-                  un choix éclairé et trouver la nourrice idéale pour leurs
-                  enfants.
+                  un choix éclairé et trouver l'assistante maternelle idéale
+                  pour leurs enfants.
                 </p>
               </div>
             </div>
